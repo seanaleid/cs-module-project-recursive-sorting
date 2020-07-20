2662e6fdb5348e9128ecaed6e38f0ab465a5ac8f
seanaleid <seanaleid@gmail.com>
2020-07-20 17:48:01 -0400

    Complete merge_sort

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -25,3 +25,13 @@ def binary_search(arr, target, start, end):
     # if the start is the same or larger than the end,
     # return false (-1)
     else: return -1
+
+# STRETCH: implement an order-agnostic binary search
+# This version of binary search should correctly find
+# the target regardless of whether the input array is 
+# sorted in ascending order or in descending order
+# You can implement this function either recursively
+# or iteratively
+def agnostic_binary_search(arr, target):
+    # Your code here
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,17 +1,36 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+def merge(left, right):
+    # elements = len(arrA) + len(arrB)
+    # merged_arr = [0] * elements
 
     # Your code here
+    merged_arr = []
+    left_pointer = 0
+    right_pointer = 0
 
+    while left_pointer < len(left) and right_pointer < len(right):
+        if left[left_pointer] < right[right_pointer]:
+            merged_arr.append(left[left_pointer])
+            left_pointer += 1
+        else:
+            merged_arr.append(right[right_pointer])
+            right_pointer += 1
 
+    merged_arr.extend(left[left_pointer:])
+    merged_arr.extend(right[right_pointer:])
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
-
+    # base case
+    if len(arr) < 2:
+        return arr
+    else:
+        middle = round(len(arr)/2)
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
+        return merge(left,right)
 
     return arr
 
@@ -21,8 +40,9 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
